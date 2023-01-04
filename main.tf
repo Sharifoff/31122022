f37fdc71db91efcf82c17fd1dc3b8569dfce011a
Sharifoff <efqanserifov@gmail.com>
2023-01-04 23:12:21 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   backend "s3" {
     bucket = "soccer31122022"
     key    = "global/s3/terraform.tfstate"
-    region = "eu-west-3"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-  region = "eu-west-3"
+  region = "us-east-1"
 }
 
 
