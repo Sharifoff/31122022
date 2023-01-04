2d5eb384a06914a313521c3bd90c52f189856565
Sharifoff <efqanserifov@gmail.com>
2023-01-04 21:26:38 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   required_version = ">= 1.2.0"
 
   backend "s3" {
-    bucket = "soccer-bucket-terraform"
+    bucket = "soccer31122022"
     key    = "global/s3/terraform.tfstate"
     region = "us-east-1"
   }
