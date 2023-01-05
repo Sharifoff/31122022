830025f6765622baf58ea6358e1282a304fd0674
Sharifoff <efqanserifov@gmail.com>
2023-01-05 13:10:13 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,12 @@ resource "aws_default_vpc" "default" {
       Name = "Default VPC"
     }
   }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
