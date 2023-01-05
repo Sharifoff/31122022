c54c88af0ca11f09828825b8eb44ee266e3c70a8
Sharifoff <efqanserifov@gmail.com>
2023-01-05 13:41:50 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,15 @@ resource "aws_default_vpc" "default" {
       Name = "Default VPC"
     }
   }
+
+
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "WebServer08122022"
+
+  }
+}
