974f8c5957ea74f56d722d1a78417fcb6b0955be
Sharifoff <efqanserifov@gmail.com>
2023-01-06 14:01:04 +0400

    commit

--- main.tf
+++ main.tf
@@ -30,5 +30,14 @@ resource "aws_instance" "app_server" {
   }
 }
 
+resource "aws_instance" "web_server" {
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web08122022"
+
+  }
+}
 
 #deyissdfghdfghggsd
