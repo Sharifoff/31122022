19448fce01500173c97f7873aba1516d4ac2097e
Sharifoff <efqanserifov@gmail.com>
2023-01-06 13:13:17 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,5 +10,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
 
+  tags = {
+    Name = "AppServer08122022"
+
+  }
+}
 #deyissdfghdfghggsdasdfghjk
