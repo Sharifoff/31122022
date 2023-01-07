c2cb4412363abfb68587281e3ceb3a370801287f
Sharifoff <efqanserifov@gmail.com>
2023-01-07 21:47:57 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,20 +14,11 @@ provider "aws" {
 
 resource "aws_default_vpc" "default" {
     tags = {
-      Name = "yeni VPC"
+      Name = "teze VPC"
     }
   }
 
 
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0b0dcb5067f052a63"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "App08122022"
-
-  }
-}
 
 #deyissdfghdfghggsdfhf
